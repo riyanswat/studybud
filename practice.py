5b0122447793e8fb492aa02baeedc1840ee684fc
Rayan Khan <riyanswat@gmail.com>
2024-01-09 00:20:55 +0500

    initial commit

---
+++ practice.py
@@ -0,0 +1,15 @@
+# # from pathlib import Path
+
+# # BASE_DIR = Path(__file__).resolve().parent.parent
+# # print("BASE_DIR:", BASE_DIR)
+# # print("BASE_DIR / 'templates':", BASE_DIR / 'templates')
+
+
+# import sqlite3
+# conn = sqlite3.connect("db.sqlite3")
+# c = conn.cursor()
+
+# c.execute("SELECT * from auth_group")
+# rows = c.fetchall()
+
+# print(rows)
