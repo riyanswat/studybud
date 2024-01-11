9b3571d048b68bc772fb2edb68095a3f4a3be5fc
Rayan Khan <riyanswat@gmail.com>
2024-01-12 01:06:36 +0500

    ..

--- practice.py
+++ practice.py
@@ -13,3 +13,29 @@
 # rows = c.fetchall()
 
 # print(rows)
+
+
+# import random
+
+# names = ["Roman Reigns", "Seth Rollins", "Dean Ambrose", "John Cena",
+#          "Kane", "The Undertaker", "Triple H", "Shawn Michaels", "C M Punk"]
+
+# for n in range(len(names)):
+#     name = random.choice(names)
+#     names.pop(names.index(name))
+#     print(name)
+
+
+# class Matrix:
+#     def __init__(self, data):
+#         self.data = data
+
+#     def __and__(self, other):
+#         # Implement matrix multiplication
+#         return f"{self.data} {other}"
+
+
+# mat = Matrix("Riyan")
+# result_matrix = mat & "khan"
+
+# print(result_matrix)
