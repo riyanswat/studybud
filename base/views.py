a6c97771b5651f090b34d7835c9273ccfafa0fd5
Rayan Khan <riyanswat@gmail.com>
2024-01-13 00:40:08 +0500

    ...

--- base/views.py
+++ base/views.py
@@ -12,6 +12,11 @@ from .forms import RoomForm
 # ]
 
 
+def loginPage(request):  # dont use just 'login' cuz theres a builtin login func
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ""
     rooms = Room.objects.filter(
